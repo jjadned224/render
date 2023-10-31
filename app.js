96ee5ffd60ef95922e421f55e6584ffdce4a3158
Jihadned <jjadned@gmail.com>
2023-10-31 16:32:00 -0400

    working server

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3001;
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
+    res.end("<h1 style='text-align: center'><a href='https://github.com/jjadned224/render'>Github Link!</a></h1>")
   })
   .listen(port);
